255c47547367b861eea5f6905d85d2229c205ff2
Shakir28 <33629503+Shakir28@users.noreply.github.com>
2023-07-12 19:50:01 +0530

    Text Changes in Landing Page
    Text update to my name in 'By' section in Landing Page

--- app/page.js
+++ app/page.js
@@ -6,25 +6,12 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          This is first Next JS application &nbsp;
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <p>
+            By{' '} Shakir Ali
+          </p>
         </div>
       </div>
 
